bfe91824b151e75b204a90c80568738ce0b2dcfb
Joy Lin <vvlnote@hotmail.com>
2019-02-15 07:37:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  length = array.length
+  newStr = ""
+  if length == 1
+    newStr = array.join
+  elsif length == 2
+    newStr = array.join(' and ')
+  else
+    temp = array.take(length-1)
+    newStr = temp.join(", ")
+    newStr += ", and #{array[length-1]}"
+  end
+  newStr
 end
